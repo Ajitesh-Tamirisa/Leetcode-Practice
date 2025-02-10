1a3da0dd3b61525a2d9f784880e92e9f9fd6ed44
Sathyasai Ajitesh Tamirisa <60847274+Ajitesh-Tamirisa@users.noreply.github.com>
2025-02-10 15:14:43 -0800

    Update and rename Meeting Rooms.js to 252. Meeting Rooms.js

--- Intervals/252. Meeting Rooms.js
+++ Intervals/252. Meeting Rooms.js
@@ -1,4 +1,5 @@
 // https://neetcode.io/problems/meeting-schedule
+// https://leetcode.com/problems/meeting-rooms/
 
 // Meeting Rooms
 // Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), determine if a person could add all meetings to their schedule without any conflicts.
